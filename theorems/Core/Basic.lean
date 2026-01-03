8aa103dcd00fbbaee07243fc1a10d8d80c86b638
l1mey112 <l-m@l-m.dev>
2026-01-04 10:19:03 +1100

    theorems: move lemma files around

---
+++ theorems/Core/Basic.lean
@@ -0,0 +1,3 @@
+import theorems.Core.Basic.basic_binary
+import theorems.Core.Basic.basic_bitwise
+import theorems.Core.Basic.basic_cmp
--- theorems/Core/Basic/basic_bitwise.lean
+++ theorems/Core/Basic/basic_bitwise.lean
@@ -16,10 +16,10 @@ import theorems.iN
 @[simp high] theorem poison_ashr_eq_poison : poison >>ₛ x = poison := by cases x <;> rfl
 @[simp high] theorem ashrExact_poison_eq_poison : x >>ₛexact poison = poison := by cases x <;> rfl
 @[simp high] theorem poison_ashrExact_eq_poison : poison >>ₛexact x = poison := by cases x <;> rfl
-@[simp high] theorem and_poison_eq_poison : x & poison = poison := by cases x <;> rfl
-@[simp high] theorem poison_and_eq_poison : poison & x = poison := by cases x <;> rfl
-@[simp high] theorem or_poison_eq_poison : x | poison = poison := by cases x <;> rfl
-@[simp high] theorem poison_or_eq_poison : poison | x = poison := by cases x <;> rfl
-@[simp high] theorem not_poison_eq_poison : ~(poison : iN n) = poison := by rfl
-@[simp high] theorem xor_poison_eq_poison : x ^' poison = poison := by cases x <;> rfl
-@[simp high] theorem poison_xor_eq_poison : poison ^' x = poison := by cases x <;> rfl
+@[simp high] theorem and_poison_eq_poison : (x : iN n) &&& (poison : iN n) = poison := by cases x <;> rfl
+@[simp high] theorem poison_and_eq_poison : (poison : iN n) &&& (x : iN n) = poison := by cases x <;> rfl
+@[simp high] theorem or_poison_eq_poison : (x : iN n) ||| (poison : iN n) = poison := by cases x <;> rfl
+@[simp high] theorem poison_or_eq_poison : (poison : iN n) ||| (x : iN n) = poison := by cases x <;> rfl
+@[simp high] theorem not_poison_eq_poison : ~~~(poison : iN n) = poison := by rfl
+@[simp high] theorem xor_poison_eq_poison : (x : iN n) ^^^ (poison : iN n) = poison := by cases x <;> rfl
+@[simp high] theorem poison_xor_eq_poison : (poison : iN n) ^^^ (x : iN n) = poison := by cases x <;> rfl
